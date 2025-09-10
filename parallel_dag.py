acd28417027536424681ad12095d470cece4d6b7
PODS <62228049+sakethpodila@users.noreply.github.com>
2025-09-10 22:13:14 +0530

    Created parallel_dag.py

---
+++ parallel_dag.py
@@ -0,0 +1,26 @@
+from airflow.decorators import dag, task
+from airflow.models.baseoperator import chain
+from time import sleep
+
+@dag(schedule=None)
+def parallel_dag():
+  @task
+  def task_1():
+    sleep(30)
+    
+  @task
+  def task_2():
+    sleep(30)
+
+  @task
+  def task_3():
+    sleep(30)
+
+  @task
+  def task_4():
+    print('Done')
+
+
+  chain([task_1(), task_2(), task_3(), task_4())
+
+parallel_dag()
